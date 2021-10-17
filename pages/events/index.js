89736c310b87a985a0563cf92298cf8efe7d70f9
Jakub Widurek <jakubwidurek@gmail.com>
2021-10-17 23:35:34 +0200

    add event-search components with styles

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,5 +1,16 @@
+import { getAllEvents } from '../../dummy-data';
+import EventList from '../../components/events/event-list';
+import EventSearch from '../../components/events/events-search';
+
 function AllEvents() {
-  return <div>Events Page</div>;
+  const events = getAllEvents();
+
+  return (
+    <div>
+      <EventSearch />
+      <EventList items={events} />
+    </div>
+  );
 }
 
 export default AllEvents;
