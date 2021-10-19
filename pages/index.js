c12f2081e342c6852aeeae57f9dbf996e440478b
Jakub Widurek <jakubwidurek@gmail.com>
2021-10-19 21:38:39 +0200

    create api-util file to fetch data. Create getStaticProps function to pre-render fetching dataon page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,24 @@
-import { getFeaturedEvents } from '../dummy-data';
+import { getFeaturedEvents } from '../helpers/api-util';
 import EventList from '../components/events/event-list';
 
-const HomePage = () => {
+const HomePage = ({ events }) => {
   const featureEvents = getFeaturedEvents();
 
   return (
     <div>
-      <EventList items={featureEvents} />
+      <EventList items={events} />
     </div>
   );
 };
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+  };
+}
 
 export default HomePage;
