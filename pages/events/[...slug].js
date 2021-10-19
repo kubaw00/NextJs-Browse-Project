4c1a2f4b994d59d01541fcd6e9ab9a93fe7798f6
Jakub Widurek <jakubwidurek@gmail.com>
2021-10-20 01:16:59 +0200

    add getServerSideProps function to [...slug].js

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,38 +1,33 @@
 import React from 'react';
 import { useRouter } from 'next/router';
-import { getFilteredEvents } from '../../dummy-data';
+import { getFilteredEvents } from '../../helpers/api-util';
 import EventList from '../../components/events/event-list';
 import ResultsTitle from '../../components/events/results-title';
 import Button from '../../components/ui/button';
 import ErrorAlert from '../../components/ui/error-alert';
 
-function FilteredEventsPage() {
-  const filteredData = useRouter().query.slug;
+function FilteredEventsPage(props) {
+  // const filteredData = useRouter().query.slug;
 
-  if (!filteredData) {
-    return <p className="center">Loading...</p>;
+  // if (!filteredData) {
+  //   return <p className="center">Loading...</p>;
+  // }
+
+  if (props.hasError) {
+    return (
+      <>
+        <ErrorAlert>
+          <p>Invalit filter, please adjust yout values!</p>
+        </ErrorAlert>
+
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </>
+    );
   }
 
-  const filteredYear = +filteredData[0];
-  const filteredMonth = +filteredData[1];
-
-  if (
-    isNaN(filteredYear) ||
-    isNaN(filteredMonth) ||
-    filteredYear > 2030 ||
-    filteredYear < 2021 ||
-    filteredMonth < 1 ||
-    filteredMonth > 12
-  ) {
-    return <p>Invalit filter, please adjust yout values!</p>;
-  }
-
-  const filteredEvents = getFilteredEvents({
-    year: filteredYear,
-    month: filteredMonth,
-  });
-
-  console.log(filteredEvents);
+  const filteredEvents = props.events;
 
   if (!filteredEvents || filteredEvents.length === 0) {
     return (
@@ -48,7 +43,7 @@ function FilteredEventsPage() {
     );
   }
 
-  const date = new Date(filteredYear, filteredMonth - 1);
+  const date = new Date(props.date.year, props.date.month - 1);
 
   return (
     <>
@@ -56,6 +51,49 @@ function FilteredEventsPage() {
       <EventList items={filteredEvents} />
     </>
   );
+}
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+
+  const filteredData = params.slug;
+
+  const filteredYear = +filteredData[0];
+  const filteredMonth = +filteredData[1];
+
+  if (
+    isNaN(filteredYear) ||
+    isNaN(filteredMonth) ||
+    filteredYear > 2030 ||
+    filteredYear < 2021 ||
+    filteredMonth < 1 ||
+    filteredMonth > 12
+  ) {
+    return {
+      props: {
+        hasError: true,
+      },
+
+      // notFound: true,
+      // redirect: {
+      //   destination: '/error'
+      // }
+    };
+  }
+  const filteredEvents = await getFilteredEvents({
+    year: filteredYear,
+    month: filteredMonth,
+  });
+
+  return {
+    props: {
+      events: filteredEvents,
+      date: {
+        year: filteredYear,
+        month: filteredMonth,
+      },
+    },
+  };
 }
 
 export default FilteredEventsPage;
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,10 @@
-import { getAllEvents } from '../../dummy-data';
+import { getAllEvents } from '../../helpers/api-util';
 import EventList from '../../components/events/event-list';
 import EventSearch from '../../components/events/events-search';
-
 import { useRouter } from 'next/router';
 
-function AllEvents() {
+function AllEvents({ events }) {
   const router = useRouter();
-  const events = getAllEvents();
 
   function findEventHandler(year, month) {
     const fullPath = `/events/${year}/${month}`;
@@ -20,6 +18,17 @@ function AllEvents() {
       <EventList items={events} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events,
+    },
+    revalidate: 60,
+  };
 }
 
 export default AllEvents;
