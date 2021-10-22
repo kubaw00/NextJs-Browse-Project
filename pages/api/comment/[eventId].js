85e27bd4d546530947ed137c91d44e56c68790cb
Jakub Widurek <jakubwidurek@gmail.com>
2021-10-22 15:46:16 +0200

    add coments api routes

---
+++ pages/api/comment/[eventId].js
@@ -0,0 +1,34 @@
+function handler(req, res) {
+  const eventId = req.query.eventId;
+  if (req.method === 'POST') {
+    const { email, name, text } = req.body;
+
+    if (
+      !email.includes('@') ||
+      !name ||
+      name.trim() === '' ||
+      !text ||
+      text.trim() === ''
+    ) {
+      res.status(422).json({ message: 'Invalid input' });
+      return;
+    }
+    const newComment = {
+      id: new Date().toISOString(),
+      email,
+      name,
+      text,
+    };
+
+    res.status(201).json({ message: 'Added comment. ', comment: newComment });
+  }
+
+  if (req.method === 'GET') {
+    const dummyList = [
+      { id: 'c1', name: 'Jakub', comment: 'A first Comment' },
+      { id: 'c2', name: 'MAx', comment: 'A second Comment' },
+    ];
+    res.status.json({ comments: dummyList });
+  }
+}
+export default handler;
